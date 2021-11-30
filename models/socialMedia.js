10e5ba6d17c6d1d13f264dd3bf88d20914888108
HafidurR <deifah126@gmail.com>
2021-11-30 09:04:13 +0700

    add models

---
+++ models/socialMedia.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+  const SocialMedia = sequelize.define('SocialMedia', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Nama tidak boleh kosong'
+          }
+        }
+      },
+      social_media_url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Social_media_url tidak boleh kosong'
+          }
+        }
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+  },{
+    tableName: 'Social_Media'
+  })
+
+  SocialMedia.associate = models => {
+    SocialMedia.belongsTo(models.User, {
+      foreignKey: 'id'
+    });
+  }
+
+  return SocialMedia;
+}
