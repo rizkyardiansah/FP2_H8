89299d7ff1252d44ec423170ab446d2ba2ae4fd1
HafidurR <deifah126@gmail.com>
2021-11-30 09:03:34 +0700

    add migration for socialMedia

---
+++ migrations/20211129121948-SocialMedia.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Social_Media', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      social_media_url: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('Social_Media');
+  }
+};
