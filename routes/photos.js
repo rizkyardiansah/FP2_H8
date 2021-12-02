f98a155cf1fb0daa5cfb18ff59c20d80e537193b
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-12-02 22:37:37 +0700

    menambahkan controller/photo/read

--- routes/photos.js
+++ routes/photos.js
@@ -3,6 +3,7 @@ const photoRoute = require("../controller/photo");
 const verifyToken = require("../middleware/verifyToken");
 
 router.post("/", verifyToken, photoRoute.create);
+router.get('/', verifyToken, photoRoute.read)
 router.put('/:photoId', verifyToken, photoRoute.edit)
 router.delete('/:photoId', verifyToken, photoRoute.destroy)
 
