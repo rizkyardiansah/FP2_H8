7677ba37b3572ea2f6cea34b7983b7c7ddbd2cc6
HafidurR <deifah126@gmail.com>
2021-12-10 20:07:48 +0700

    edit some endpoint

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = 3000;
-const router = require('./routes/users');
+const express     = require('express');
+const app         = express();
+const port        = 3000;
+const users       = require('./routes/users');
+const socialmedia = require('./routes/socialMedia');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use('/api/users', router)
+app.use('/api/users', users)
+app.use('/api/socialmedias', socialmedia)
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
--- controller/user/Edit.js
+++ controller/user/Edit.js
@@ -12,35 +12,48 @@ module.exports = async (req, res) => {
     phone_number
   } = req.body;
   const id = req.params.id;
+  const UserId = req.user.id;
+  // return res.json({
+  //   id, UserId
+  // })
   //return console.log(User);
-  await User.update({
-    full_name,
-    email,
-    username,
-    profile_image_url,
-    age,
-    phone_number
-  }, {
-    where: {
-      id: id
-    }
-  }).then(result => {
-    if (result == 1) {
-      return res.status(200).json({
-        status: 'success',
-        message: 'Success changed',
-        user: result
-      })
-    } else {
-      return res.status(404).json({
-        status: 'error',
-        message: 'User not found'
-      })
-    }
-  }).catch(error => {
-    return res.status(400).json({
-      status: 'error',
-      message: error
+  if (parseInt(id) !== UserId) {
+    return res.status(401).json({
+      status: "error",
+      message: "user unauthorized"
     })
-  })
+  } else {
+    await User.update({
+      full_name,
+      email,
+      username,
+      profile_image_url,
+      age,
+      phone_number
+    }, {
+      where: {
+        id: id
+      }
+    }).then(result => {
+      //return console.log(result);  
+      if (result == 1) {
+        res.status(200).json({
+          status: 'success',
+          message: 'Success changed',
+          user: req.body
+        })
+      } else {
+        return res.status(404).json({
+          status: 'error',
+          message: 'User not found'
+        })
+      }
+    }).catch(error => {
+      return res.status(400).json({
+        status: 'error',
+        message: error
+      })
+    })
+  }
+
 }
