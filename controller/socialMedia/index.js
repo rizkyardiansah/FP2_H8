988b4028dc9d12c58d08b4bd5bac75975ab268e2
HafidurR <deifah126@gmail.com>
2021-12-13 20:01:29 +0700

    edit some route socialmedia

--- controller/socialMedia/index.js
+++ controller/socialMedia/index.js
@@ -1,7 +1,11 @@
 const create = require('./Create');
 const getAll = require('./Get');
+const edit   = require('./Edit');
+const destroy = require('./Delete');
 
 module.exports = {
   create,
-  getAll
+  getAll,
+  edit,
+  destroy
 }
--- routes/socialMedia.js
+++ routes/socialMedia.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const socialMediaRouter = require('../controller/socialMedia');
 const verifytoken = require('../middleware/verifyToken');
-const { route } = require('./users');
-
 
 router.get('/', verifytoken, socialMediaRouter.getAll);
 router.post('/', verifytoken, socialMediaRouter.create);
+router.put('/:id', verifytoken, socialMediaRouter.edit);
+router.delete('/:id', verifytoken, socialMediaRouter.destroy);
 
 
 module.exports = router;
